7fbd9d2deba07887306e29c70f7449f9f571adbd
Wesley Jacobs <wesleyjacobs@Wesleys-MacBook-Pro.local>
2019-09-20 11:42:32 -0700

    created STORE object

---
+++ script.js
@@ -0,0 +1,60 @@
+'use strict';
+
+// Store is the questions, answers, correct answers, and score.
+// Store is a global object.
+const STORE = {
+  questions: [//1
+    {
+      question: 'What did Jim get Pam as a secret Santa gift?',
+      answerOptions: [
+        'An iPod', 
+        'A book', 
+        'A teapot', 
+        'A toothbrush'
+      ],
+      correctAnswer: 'A teapot'
+    },
+    { //2
+      question: 'Kevin\'s family has a secret family recipie for?',
+      answerOptions: [
+        'Lasagna', 
+        'Chilli', 
+        'Tacos', 
+        'Meatballs'
+      ],
+      correctAnswer: 'Chilli'
+    },
+    { //3
+      question: 'What is the name of Jan\'s talanted assistant?',
+      answerOptions: [
+        'David Wallace', 
+        'Pete', 
+        'Clayton', 
+        'Hunter'
+      ],
+      correctAnswer: 'Hunter'
+    },
+    { //4
+      question: 'The office olympics included what sport?',
+      answerOptions: [
+        'Table-tennis', 
+        'Sputnick', 
+        'Flonkerton', 
+        'Blomp-top'
+      ],
+      correctAnswer: 'Flonkerton'
+    },
+    { //5
+      question: 'This quiz is over, "catch you on..."?',
+      answerOptions: [
+        '...the flip side.', 
+        '...the flippity flop.', 
+        '...the other side.', 
+        'Toothbrush.'
+      ],
+      correctAnswer: '...the flippity flop.'
+    }
+  ]
+};
+    
+  
